84ee8c00c14060c9a2293ff71761ac27f12df7b7
Teison Montero <teisonmontero@MacBook-Air-de-Teison.local>
2019-11-03 12:11:00 -0400

    refactor: teble.js

--- js/table.js
+++ js/table.js
@@ -1,62 +1,33 @@
-  //Imprimir
-  let imprimir = document.getElementById('imprimir')
-  .addEventListener('click', () =>{
-    let result = document.querySelector('.result');
-    let multiplicando = document.getElementById('n-tabla');
-    let multiplicador = document.getElementById('l-multiplicar');
-    
-    //validacion de campos vacios
-      if(multiplicando.value == ''){
-        multiplicando.value = 1;
-      }
-      if(multiplicador.value == ''){
-        multiplicador.value = 12;
-      }
-    //validacion de campos vacios
+  const {document: doc} = window;
+  const imprimir = doc.getElementById('imprimir');
+  imprimir.addEventListener('click', () =>{
+    const result = doc.querySelector('.result');
+    const multiplicandoElem = doc.getElementById('n-tabla');
+    let multiplicando = parseInt( multiplicandoElem.value);
+    let multiplicador = parseInt(doc.getElementById('l-multiplicar').value);
+    let n1 = !multiplicando || multiplicando < 1 ? 1 : multiplicando;
+    let n2 = !multiplicador || multiplicador < 1 ? 12 : multiplicador;
   
-    let n1 = parseInt(multiplicando.value);
-    let n2 = parseInt(multiplicador.value);
-
-    //vaidacion de valor menor que 1 y que sean numeros
-      
-      if(n1 < 1){
-        n1 = 1;
-      }
-      if(n2 < 1){
-        n2 = 12;
-      }
-      
-      if(multiplicando.value != Number || multiplicador.value !=Number){
-        multiplicando.value = 1;
-        multiplicador.value = 12;
-      }
-    //vaidacion de valor menor que 1
-    
-    //eliminar una tabla
-    const table = document.createElement('div');
+    const table = doc.createElement('div');
     table.classList = 'element';
-    document.addEventListener('click', (e) => {
+    doc.addEventListener('click', (e) => {
       const elem = e.target;
       if (elem.classList[0] === 'element'){
         elem.remove();
       }
     });
-    //eliminar una tabla
 
-    //eliminar todas las tablas
-      eliminar = document.getElementById('eliminar')
+     doc.getElementById('eliminar')
       .addEventListener('click', (e) => {
         result.innerHTML = '';
-        multiplicando.focus();
+        multiplicandoElem.focus();
       });
-    //eliminar todas las tablas
     
     for(i = 1; i <= n2 ; i++){
-     let tabla = `
-     <p>${n1} <span>x</span> ${i} <span> = </span> ${n1 * i}</p>`
+     let tabla = `<p>${n1} <span>x</span> ${i} <span> = </span> ${n1 * i}</p>`;
      table.innerHTML += tabla;
      result.append(table);
     }
-    multiplicando.value = "";
-    multiplicando.focus();
+    multiplicandoElem.value = "";
+    multiplicandoElem.focus();
   });
