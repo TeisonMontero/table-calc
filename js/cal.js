572c0a9c3d5cc7200dd990959d974f9f5dd9a366
Teison Montero <teisonmontero@MacBook-Air-de-Teison.local>
2019-11-09 23:20:26 -0400

    refactor: cal.js

--- js/cal.js
+++ js/cal.js
@@ -1,105 +1,53 @@
-//limpiar input {
-  const clean = document.getElementById('clean')
-  .addEventListener('click', limpiarInput = () => {
-    document.getElementById("input").reset();
-    // historial.innerHTML = "";
-  });
+const {document: doc} = window;
+//input clean {
+doc.getElementById('clean').addEventListener('click', limpiarInput = () => {
+  doc.getElementById("input").reset();
+});
 // }
 
 let primerOperando = 0;
 let seguntoOperando = 0;
 let operacion = 0;
 
-// let historial = document.getElementById('historial');
-let display = document.getElementById('display');
-
-//agregar un numero
-let addNumber = (id, valor) => {
-  document.getElementById(id)
-  .addEventListener('click', dnumero = () => {
-    if(display.value == 0){
-      display.value = valor;
-    }
-    else{
-      display.value += valor;
-    }
-  });
+let operaciones = (id, signo) => {
+doc.getElementById(id).addEventListener('click', addOperacion = () => {
+  operacion = signo;
+  primerOperando = Number(display.value);
+  display.value = 0;
+});
 }
 
-let sumar = document.getElementById('sumar')
-.addEventListener('click', addOperacion = () => {
-  operaciones('+');
-  
-});
+operaciones('sumar', '+');
+operaciones('restar', '-');
+operaciones('multiplicar', 'x');
+operaciones('division', '/');
+operaciones('porciento', '%');
 
-let restar = document.getElementById('restar')
-.addEventListener('click', addOperacion = () => {
-  operaciones('-');
-  
-});
-
-let multiplicar = document.getElementById('multiplicar')
-.addEventListener('click', addOperacion = () => {
-  operaciones('x');
-  
-});
-
-let dividir = document.getElementById('division')
-.addEventListener('click', addOperacion = () => {
-  operaciones('/');
-  
-});
-
-let porciento = document.getElementById('porciento')
-.addEventListener('click', addOperacion = () => {
-  operaciones('%');
-  
-});
-
-//sumar
-
-
-//calcular
-let calcular = document.getElementById('calcular')
+doc.getElementById('calcular')
 .addEventListener('click', calcularResultado = () =>{
   if(display.value != 0){
-
     let resultado;
     seguntoOperando = Number(display.value);
     display.value = 0;
    
-    if(operacion == '+'){
-      resultado = primerOperando + seguntoOperando;
-    }
-    if(operacion == '-'){
-      resultado = primerOperando - seguntoOperando;
-    }
-    if(operacion == 'x'){
-      resultado = primerOperando * seguntoOperando;
-    }
-    if(operacion == '/'){
-      resultado = primerOperando / seguntoOperando;
-    }
-    if(operacion == '%'){
-      resultado = (primerOperando * 0.1);
-    }
+    if (operacion == '+') resultado = primerOperando + seguntoOperando;
+    if (operacion == '-') resultado = primerOperando - seguntoOperando;
+    if (operacion == 'x') resultado = primerOperando * seguntoOperando;
+    if (operacion == '/') resultado = primerOperando / seguntoOperando;
     
     display.value = resultado;
   }
   else return false;
-  
-})
+});
 
-
-function operaciones (signo){
-  operacion = signo;
-  primerOperando = Number(display.value);
-  // historial.innerHTML = `${primerOperando} ${signo} ${seguntoOperando}`;
-  display.value = 0;
-  
+//add number
+let addNumber = (id, valor) => {
+  doc.getElementById(id)
+  .addEventListener('click', dnumero = () => {
+    display.value == 0 ? display.value = valor : display.value += valor;
+  });
 }
 
-//numeros
 addNumber('n-1', 1);
 addNumber('n-2', 2);
 addNumber('n-3', 3);
